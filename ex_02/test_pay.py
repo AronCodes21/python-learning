9619ee5e7099e7d463b058ab944cc205db43c40e
Linezokode <aronk.code21@gmail.com>
2022-03-31 00:01:54 +0200

    better unittests

--- ex_02/test_pay.py
+++ ex_02/test_pay.py
@@ -1,20 +1,20 @@
 import unittest
 
-import ex_03
+import payment
 
 
 class testPayment(unittest.TestCase):
   def test_1(self):
-    result = ex_03.payment(10, 20)
+    result = payment.payment(10, 20)
 
   def test_2(self):
-    result = ex_03.payment(100, 202)
+    result = payment.payment(100, 202)
 
   def test_3(self):
-    result = ex_03.payment(6.5, 25.4)
+    result = payment.payment(6.5, 25.4)
 
   def test_4(self):
-    result = ex_03.payment(-10, -25)
+    result = payment.payment(-10, -25)
 
 if __name__ == "__main__":
   unittest.main()
